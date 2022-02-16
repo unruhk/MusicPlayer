fd2141705c3b82c5ef175dddf17671c476385482
unruhk <kunruh23@gmail.com>
2022-02-16 20:58:45 +0100

    Initial Commit

---
+++ MusicAppJared/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  MusicAppJared
+//
+//  Created by Klaus Unruh on 16.02.22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ MusicAppJared/MusicAppJaredApp.swift
@@ -0,0 +1,17 @@
+//
+//  MusicAppJaredApp.swift
+//  MusicAppJared
+//
+//  Created by Klaus Unruh on 16.02.22.
+//
+
+import SwiftUI
+
+@main
+struct MusicAppJaredApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
